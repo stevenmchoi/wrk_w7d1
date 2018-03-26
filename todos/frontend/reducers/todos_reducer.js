26983cac8ce761581ac41e3579f1ebae14d7991e
Steven Choi <steven.m.choi@gmail.com>
2018-03-26 14:20:19 -0700

    finish phase 2 for todo list

--- todos/frontend/reducers/todos_reducer.js
+++ todos/frontend/reducers/todos_reducer.js
@@ -1,10 +1,38 @@
+import {
+	RECEIVE_TODOS,
+	RECEIVE_TODO,
+	receiveTodos,
+	receiveTodo
+} from '../actions/todo_actions';
+import {merge} from 'lodash';
 
+const initialState = {
+	1: {
+		id: 1,
+		title: 'wash car',
+		body: 'with soap',
+		done: false
+	},
+	2: {
+		id: 2,
+		title: 'wash dog',
+		body: 'with shampoo',
+		done: true
+	}
+};
 
-const todosReducer = (state = {}, action) => {
-  switch(action.type) {
-    default:
-      return state;
-  }
+const todosReducer = (state = initialState, action) => {
+	Object.freeze(state);
+	// debugger;
+	switch (action.type) {
+		case RECEIVE_TODOS:
+			return action.todos;
+		case RECEIVE_TODO:
+			const newTodo = {[action.todo.id]: action.todo};
+			return merge({}, state, newTodo);
+		default:
+			return state;
+	}
 };
 
 export default todosReducer;
--- todos/frontend/todo_redux.jsx
+++ todos/frontend/todo_redux.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 
+import {receiveTodos, receiveTodo} from './actions/todo_actions';
+window.receiveTodos = receiveTodos;
+window.receiveTodo = receiveTodo;
+
 document.addEventListener('DOMContentLoaded', () => {
 	window.store = configureStore();
 	ReactDOM.render(<h1>Todos App</h1>, document.getElementById('content'));
