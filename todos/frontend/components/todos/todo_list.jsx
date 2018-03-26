c96cccfb53cd6dcbe01148fb16843c91268bfaa9
Steven Choi <steven.m.choi@gmail.com>
2018-03-26 16:16:23 -0700

    Finish Phase 3: TodoList

--- todos/frontend/components/todos/todo_list.jsx
+++ todos/frontend/components/todos/todo_list.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import TodoListItem from './todo_list_item';
 
-const TodoList = () => (
-  <h3>Todo List goes here!</h3>
+const TodoList = ({ todos }) => (
+	<div>
+		<h3>Todo List goes here!</h3>
+    <ul>
+      {todos.map((todo) => <TodoListItem todo={todo} key={todo.id}/>)}
+    </ul>
+	</div>
 );
 
 export default TodoList;
